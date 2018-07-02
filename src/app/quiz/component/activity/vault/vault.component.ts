5732e851db33956e7f3126f9bdb9a8553329cce4
Sohitri216 <sohitri.sen@learningmate.com>
2018-07-02 20:25:05 +0530

    initial structure

---
+++ src/app/quiz/component/activity/vault/vault.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vault',
+  templateUrl: './vault.component.html',
+  styles: ['./vault.component.scss']
+})
+export class VaultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/quiz/quiz.module.ts
+++ src/app/quiz/quiz.module.ts
@@ -1,12 +1,14 @@
 import { NgModule,ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivityComponent } from './component/activity/activity.component';
+import { McqComponent } from './component/activity/mcq/mcq.component';
+import { VaultComponent } from './component/activity/vault/vault.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ActivityComponent]
+  declarations: [ActivityComponent, McqComponent, VaultComponent]
 })
 export class QuizModule {
 
