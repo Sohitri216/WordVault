d7d51475833e268f5cd874bde66c4779e916e0f4
Sohitri <mithi216@gmail.com>
2018-07-07 02:52:13 +0530

    round incoorect logic

--- src/app/quiz/component/activity/mcq/data.ts
+++ src/app/quiz/component/activity/mcq/data.ts
@@ -2,34 +2,42 @@ export default class QuizData {
     static quizData: Array<any> = [{
         questionText: "The little lost sheep are in _____.",
         options: ["partner", "special", "danger", "splendid"],
-        answer: 2
+        answer: 2,
+        set:1
     }, {
         questionText: "Meg is my ____ in math.",
         options: ["partner", "special", "danger", "splendid"],
-        answer: 0
+        answer: 0,
+        set:1
     },{
         questionText: "The queen lives in a ___ castle.",
         options: ["partner", "special", "danger", "splendid"],
-        answer: 3
+        answer: 3,
+        set:2
     },{
         questionText: "My ___ and I will work together as a team.",
         options: ["partner", "special", "danger", "splendid"],
-        answer: 0
+        answer: 0,
+        set:2
     },{
         questionText: "The hat is ___ because Gran gave it to me.",
         options: ["partner", "special", "danger", "splendid"],
-        answer: 1
+        answer: 1,
+        set:2
     },{
         questionText: "Small fish may face ____ in the sea.",
         options: ["partner", "special", "danger", "splendid"],
-        answer: 2
+        answer: 2,
+        set:3
     },{
         questionText: "I thanked my host for a ____ meal.",
         options: ["partner", "special", "danger", "splendid"],
-        answer: 3
+        answer: 3,
+        set:3
     },{
         questionText: "He will get ____ glasses to help him see.",
         options: ["partner", "special", "danger", "splendid"],
-        answer: 1
+        answer: 1,
+        set:3
     }]
 }
--- src/app/quiz/component/activity/mcq/mcq.component.ts
+++ src/app/quiz/component/activity/mcq/mcq.component.ts
@@ -9,95 +9,208 @@ import QuizData from './data';
 export class McqComponent implements OnInit {
   currentPageData: any;
   currentPageNo: number = 1;
-  attempNo:number=0;
+  subRoundPageNumber:number=1;
+  attempNo: number = 0;
   quizData: Array<any> = QuizData.quizData;
+  currentSet: number;
+  nextSet: number;
+  nextSetIndex:number;
+  repeatQs: Boolean = false;
+  questionInQueue: Array<any> = [];
+
   currentSelectedOption: string;
   @ViewChildren('option') option: ElementRef;
   @ViewChildren('optionText') optionText: ElementRef;
   constructor() { }
 
   ngOnInit() {
+    this.setScreenNumber();
     this.goToNextQuestion();
   };
 
+  setScreenNumber(){
+    this.quizData.map((each,index)=>{
+      each.screenNo=index+1;
+      each.attemptState="Not attempted";
+    })
+  }
 
   selectAnswer(selectedOption, idx) {
     this.currentSelectedOption = selectedOption;
     let option = this.option['_results'];
-    option[idx].nativeElement.style = 'background-color:#fabf0f';
+    option[idx].nativeElement.style = 'background-color:#fefe00';
     option.map((each, index) => {
       if (index !== idx) {
-        option[index].nativeElement.style = 'background-color:#fff';
+        option[index].nativeElement.style = 'background-color: #ededed';
       }
     })
   };
 
-  goToNextQuestion(){
+  repeatQuestion() {
+    this.attempNo = 0;
+    // console.log('repeat true', this.currentPageNo);
+    console.log('repeat q:',this.questionInQueue);
+    this.currentPageData = this.randomizeOptions(this.questionInQueue[this.subRoundPageNumber - 1], this.questionInQueue[this.subRoundPageNumber - 1].answer);
+    if(this.questionInQueue.length>1){
+      this.currentSet = this.questionInQueue[this.subRoundPageNumber - 1].set;
+      this.nextSet = this.questionInQueue[this.subRoundPageNumber].set;
+    }
+    else{
+      this.currentSet = this.questionInQueue[this.subRoundPageNumber - 1].set;
+      this.nextSet = this.nextSetIndex;
+    }
+    
+    console.log(this.currentSet,this.nextSet);
+  }
+
+  roundComplete(){
+    console.log('Round complete');
+    // Move vault
+  }
+
+  goToNextQuestion() {
     this.attempNo=0;
+    console.log('repeat false');
     console.log(this.quizData);
     this.currentPageData = this.randomizeOptions(this.quizData[this.currentPageNo - 1], this.quizData[this.currentPageNo - 1].answer);
+    this.currentSet = this.quizData[this.currentPageNo - 1].set;
+    if(this.currentSet===this.nextSetIndex){
+      this.roundComplete();
+    }
+    this.nextSet = this.quizData[this.currentPageNo].set;
+    this.nextSetIndex=this.currentSet+1;
+    // if(this.currentSet=this)
     console.log(this.currentPageData);
   };
 
-  clearSelectedOption(){
+  clearSelectedOption() {
     let option = this.option['_results']
     option.map((each, index) => {
-        option[index].nativeElement.style = 'background-color:#fff';
+      option[index].nativeElement.style = 'background-color:#ededed';
     })
   };
 
-  clearSelectedText(){
+  clearSelectedText() {
     let optionText = this.optionText['_results'];
     optionText.map((each, index) => {
-        optionText[index].nativeElement.style = 'color:#111';
+      optionText[index].nativeElement.style = 'color:#111';
     });
   };
 
-   showCorrectAnswer(){
-    let correctAns=this.currentPageData.options[this.currentPageData.answer];
+  showCorrectAnswer() {
+    let correctAns = this.currentPageData.options[this.currentPageData.answer];
     let optionText = this.optionText['_results'];
 
     optionText.map((each, index) => {
       if (each.nativeElement.innerText.toString().trim() === correctAns.toString().trim()) {
         optionText[index].nativeElement.style = 'color:#58ff00';
       }
-      if(each.nativeElement.innerText.toString().trim()=== this.currentSelectedOption.toString().trim()){
+      if (each.nativeElement.innerText.toString().trim() === this.currentSelectedOption.toString().trim()) {
         optionText[index].nativeElement.style = 'color:#ed1c24';
       }
     });
-    
-    
-    setTimeout(()=>{
+    setTimeout(() => {
       this.clearSelectedText();
-      this.goToNextQuestion();
-    },1000)
-   }
+      // this.goToNextQuestion();
+
+    }, 600);
+    this.checkQuestionSet();
+  }
+
+
+
+  checkQuestionSet() {
+    console.log('set:::',this.currentSet,this.nextSet);
+    // if(repeatFlag){
+    //   // if(this.currentSet===this.nextSetIndex)
+    // }else{
+
+    // }
+    if (this.currentSet === this.nextSet) {
+      this.currentPageNo++;
+      setTimeout(() => {
+        this.goToNextQuestion();
+      }, 1200)
+
+      console.log('same set, set:', this.currentSet)
+    }
+    else {
+      console.log('Different set:', this.nextSet,this.questionInQueue);
+      // For correct all correct set
+      if (this.questionInQueue.length === 0) {
+        this.currentPageNo++;
+        // this.repeatQs = false;
+        this.goToNextQuestion();
+        //increase footer color count
+      }
+      else {
+        // this.repeatQs = true;
+        // this.currentPageNo = this.currentPageNo - this.questionInQueue.length;
+        setTimeout(() => {
+          this.repeatQuestion();
+        }, 1200)
+        //again go to same set question one after one
+      }
+    }
+  }
 
   submit() {
+    let spliceIndex:number;
+    let matchedFLag:Boolean=false;
     if (this.currentSelectedOption === this.currentPageData.options[this.currentPageData.answer]) {
+      if(this.currentPageData.attemptState==='incorrect'){
+        
+        this.questionInQueue.map((each,idx)=>{
+          if(each.questionText===this.currentPageData.questionText){
+            spliceIndex=idx;
+          }
+        });
+        this.questionInQueue.splice(spliceIndex,1);
+        console.log(this.questionInQueue);
+        this.repeatQs=true;
+        this.checkQuestionSet();
+      }
+      else{
+        console.log('not attempted question made correct');
+        this.checkQuestionSet();
+      }
+      this.currentPageData.attemptState = "correct";
       console.log('correct');
-      this.currentPageNo++;
-      this.goToNextQuestion();
+      // this.currentPageNo++;
+     
+      // this.goToNextQuestion();
     } else {
       console.log('incorrect');
-      if(this.attempNo<2){
+      this.questionInQueue.map((each,idx)=>{
+        if(each.questionText===this.currentPageData.questionText){
+          matchedFLag=true;
+        }
+      });
+      if (this.attempNo < 2) {
         this.attempNo++;
-        if(this.attempNo===2){
+        if (this.attempNo === 2) {
+         
+          this.currentPageData.attemptState = "incorrect"
+          if(!matchedFLag){
+            this.questionInQueue.push(this.currentPageData);
+          }
+         
           this.showCorrectAnswer();
-          this.currentPageNo++;
+
+
           /**
            * store the incorrect question, to check for vault opening
            */
-          
+
         }
         // this.getCurrentQuestion();
       }
-      else{
+      else {
         this.goToNextQuestion();
       }
     }
     this.clearSelectedOption();
-   
+
   };
 
   randomizeOptions(eachObj, correctOption): any {
@@ -127,6 +240,7 @@ export class McqComponent implements OnInit {
 
     eachObj.answer = correctIndex;
     eachObj.options = optionArray;
+    // eachObj.attemptState = "Not attempted";
     return eachObj;
   };
 
