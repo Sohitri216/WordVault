6bc8951db6a4d9f4408330b53f15cfc0df05a0ee
Sohitri <mithi216@gmail.com>
2018-07-06 18:30:19 +0530

    correct, incorrect validation done

--- src/app/quiz/component/activity/mcq/data.ts
+++ src/app/quiz/component/activity/mcq/data.ts
@@ -1,11 +1,35 @@
 export default class QuizData {
     static quizData: Array<any> = [{
-        questionText: "Dummy Text1",
-        options: ["optionA", "optionB", "optionC", "optionD"],
-        answer: 1
+        questionText: "The little lost sheep are in _____.",
+        options: ["partner", "special", "danger", "splendid"],
+        answer: 2
     }, {
-        questionText: "Dummy Text2",
-        options: ["optionA", "optionB", "optionC", "optionD"],
+        questionText: "Meg is my ____ in math.",
+        options: ["partner", "special", "danger", "splendid"],
         answer: 0
+    },{
+        questionText: "The queen lives in a ___ castle.",
+        options: ["partner", "special", "danger", "splendid"],
+        answer: 3
+    },{
+        questionText: "My ___ and I will work together as a team.",
+        options: ["partner", "special", "danger", "splendid"],
+        answer: 0
+    },{
+        questionText: "The hat is ___ because Gran gave it to me.",
+        options: ["partner", "special", "danger", "splendid"],
+        answer: 1
+    },{
+        questionText: "Small fish may face ____ in the sea.",
+        options: ["partner", "special", "danger", "splendid"],
+        answer: 2
+    },{
+        questionText: "I thanked my host for a ____ meal.",
+        options: ["partner", "special", "danger", "splendid"],
+        answer: 3
+    },{
+        questionText: "He will get ____ glasses to help him see.",
+        options: ["partner", "special", "danger", "splendid"],
+        answer: 1
     }]
 }
--- src/app/quiz/component/activity/mcq/mcq.component.ts
+++ src/app/quiz/component/activity/mcq/mcq.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import  QuizData  from './data';
+import { Component, ViewChild, ViewChildren, ElementRef, OnInit } from '@angular/core';
+import QuizData from './data';
 
 @Component({
   selector: 'app-mcq',
@@ -7,15 +7,127 @@ import  QuizData  from './data';
   styleUrls: ['./mcq.component.scss']
 })
 export class McqComponent implements OnInit {
-  currentPageData: Array<any>;
-  currentPageNo: number=1;
-  quizData: Array<any>=QuizData.quizData;
+  currentPageData: any;
+  currentPageNo: number = 1;
+  attempNo:number=0;
+  quizData: Array<any> = QuizData.quizData;
+  currentSelectedOption: string;
+  @ViewChildren('option') option: ElementRef;
+  @ViewChildren('optionText') optionText: ElementRef;
   constructor() { }
 
   ngOnInit() {
+    this.goToNextQuestion();
+  };
+
+
+  selectAnswer(selectedOption, idx) {
+    this.currentSelectedOption = selectedOption;
+    let option = this.option['_results'];
+    option[idx].nativeElement.style = 'background-color:#fabf0f';
+    option.map((each, index) => {
+      if (index !== idx) {
+        option[index].nativeElement.style = 'background-color:#fff';
+      }
+    })
+  };
+
+  goToNextQuestion(){
+    this.attempNo=0;
     console.log(this.quizData);
-    this.currentPageData = this.quizData[this.currentPageNo-1];
+    this.currentPageData = this.randomizeOptions(this.quizData[this.currentPageNo - 1], this.quizData[this.currentPageNo - 1].answer);
     console.log(this.currentPageData);
-  }
+  };
+
+  clearSelectedOption(){
+    let option = this.option['_results']
+    option.map((each, index) => {
+        option[index].nativeElement.style = 'background-color:#fff';
+    })
+  };
+
+  clearSelectedText(){
+    let optionText = this.optionText['_results'];
+    optionText.map((each, index) => {
+        optionText[index].nativeElement.style = 'color:#111';
+    });
+  };
+
+   showCorrectAnswer(){
+    let correctAns=this.currentPageData.options[this.currentPageData.answer];
+    let optionText = this.optionText['_results'];
+
+    optionText.map((each, index) => {
+      if (each.nativeElement.innerText.toString().trim() === correctAns.toString().trim()) {
+        optionText[index].nativeElement.style = 'color:#58ff00';
+      }
+      if(each.nativeElement.innerText.toString().trim()=== this.currentSelectedOption.toString().trim()){
+        optionText[index].nativeElement.style = 'color:#ed1c24';
+      }
+    });
+    
+    
+    setTimeout(()=>{
+      this.clearSelectedText();
+      this.goToNextQuestion();
+    },1000)
+   }
+
+  submit() {
+    if (this.currentSelectedOption === this.currentPageData.options[this.currentPageData.answer]) {
+      console.log('correct');
+      this.currentPageNo++;
+      this.goToNextQuestion();
+    } else {
+      console.log('incorrect');
+      if(this.attempNo<2){
+        this.attempNo++;
+        if(this.attempNo===2){
+          this.showCorrectAnswer();
+          this.currentPageNo++;
+          /**
+           * store the incorrect question, to check for vault opening
+           */
+          
+        }
+        // this.getCurrentQuestion();
+      }
+      else{
+        this.goToNextQuestion();
+      }
+    }
+    this.clearSelectedOption();
+   
+  };
+
+  randomizeOptions(eachObj, correctOption): any {
+    let correctIndex: number;
+    let optionArray: string[] = eachObj.options;
+    let correctAns: string = eachObj.options[correctOption];
+    let currentIndex = optionArray.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = optionArray[currentIndex];
+      optionArray[currentIndex] = optionArray[randomIndex];
+      optionArray[randomIndex] = temporaryValue;
+    }
+
+    eachObj.options.map((each, idx) => {
+      if (each === correctAns) {
+        correctIndex = idx;
+      }
+    });
+
+    eachObj.answer = correctIndex;
+    eachObj.options = optionArray;
+    return eachObj;
+  };
 
 }
